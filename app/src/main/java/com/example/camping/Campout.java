46d6727b92b011af36d656a5a69e3bf01e3c668a
Lizzie <lizziemeis924@gmail.com>
2023-01-12 12:45:33 -0600

    week 1 updates

---
+++ app/src/main/java/com/example/camping/Campout.java
@@ -0,0 +1,17 @@
+package com.example.camping;
+
+import java.util.List;
+
+public class Campout {
+    //attributes
+    String name;
+    List<Participant> participantList;
+
+    //constructor
+    public Campout(String n, List<Participant> pl) {
+        name = n;
+        participantList = pl;
+    }
+
+    //methods
+}
---
+++ app/src/main/java/com/example/camping/Participant.java
@@ -0,0 +1,9 @@
+package com.example.camping;
+
+public class Participant {
+    //attributes
+
+    //constructor
+
+    //methods
+}
